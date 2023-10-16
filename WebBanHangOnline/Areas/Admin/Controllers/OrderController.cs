96a83b398db24792b2b62ec111796b94cefbcd55
quocdung2603 <2124802010008@student.tdmu.edu.vn>
2023-10-16 14:06:59 +0700

    16/10/23 2:06 PM fix Admin layout

--- WebBanHangOnline/Areas/Admin/Controllers/OrderController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/OrderController.cs
@@ -15,7 +15,7 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Admin/Order
-        public ActionResult Index(int ?page)
+        public ActionResult Index(int ?page, string Searchtext, string Cod, string Banking, string Paid, string UnPaid)
         {
             var pageSize = 10;
             if(page == null)
@@ -23,6 +23,28 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
                 page = 1;
             }    
             IEnumerable<Order> items= db.Orders.OrderByDescending(x => x.CreatedDate).ToList();
+            //search
+            if (!string.IsNullOrEmpty(Searchtext))
+            {
+                items = items.Where(x => x.Code.Contains(Searchtext) || x.CustomerName.Contains(Searchtext) || x.Phone.Contains(Searchtext));
+            }
+            //lọc
+            if(!string.IsNullOrEmpty(Cod) && Cod=="true")
+            {
+                items = items.Where(x => x.TypePayment == 1);
+            }    
+            else if(!string.IsNullOrEmpty(Banking) && Banking == "true")
+            {
+                items = items.Where(x => x.TypePayment == 2);
+            }
+            else if(!string.IsNullOrEmpty(UnPaid) && UnPaid == "true")
+            {
+                items = items.Where(x => x.Status == "1");
+            }    
+            else if(!string.IsNullOrEmpty(Paid) && Paid == "true")
+            {
+                items = items.Where(x => x.Status == "2");
+            }    
             var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
             items = items.ToPagedList(pageIndex, pageSize);
             ViewBag.PageSize = pageSize;
@@ -43,14 +65,14 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public ActionResult UpdateTT(int id, int trangthai)
+        public ActionResult UpdateTT(int id)
         {
             var item = db.Orders.Find(id);
             if(item!=null)
             {
                 db.Orders.Attach(item);
-                item.TypePayment = trangthai;
-                db.Entry(item).Property(x => x.TypePayment).IsModified = true;
+                item.Status = "2";
+                db.Entry(item).Property(x => x.Status).IsModified = true;
                 db.SaveChanges();
                 return Json(new { message = "Thành Công", success = true});
             }
