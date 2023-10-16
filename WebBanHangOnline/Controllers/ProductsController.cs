12d5cec842dffded27eb78933f211d9bb9429cc4
quocdung2603 <2124802010008@student.tdmu.edu.vn>
2023-10-16 09:20:32 +0700

    16/10/23 9:20 AM : lọc theo tiêu chí

--- WebBanHangOnline/Controllers/ProductsController.cs
+++ WebBanHangOnline/Controllers/ProductsController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+using PagedList;
+using PagedList.Mvc;
 
 namespace WebBanHangOnline.Controllers
 {
@@ -11,16 +14,39 @@ namespace WebBanHangOnline.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Products
-        public ActionResult Index(/*int? id*/)
+        public ActionResult Index(string Searchtext, int? page, string IsHot, string IsSale, string IsFeature)
         {
-            var items = db.Products.ToList();
-            /*if (id != null)
+            var pageSize = 8;
+            IEnumerable<Product> items = db.Products.OrderByDescending(x => x.CreatedDate);
+            if (page == null)
             {
-                items = items.Where(x => x.ProductCategoryId == id).ToList();
-            }*/
+                page = 1;
+            }
+            //khung tìm kiếm
+            if (!string.IsNullOrEmpty(Searchtext))
+            {
+                items = items.Where(x => x.Alias.Contains(Searchtext) || x.Title.Contains(Searchtext));
+            } 
+            //Lọc theo tiêu chí
+            if(!string.IsNullOrEmpty(IsHot) && IsHot =="true")
+            {
+                items = items.Where(x => x.IsHot == true);
+            }
+            if (!string.IsNullOrEmpty(IsSale) && IsSale == "true")
+            {
+                items = items.Where(x => x.IsSale == true);
+            }
+            if (!string.IsNullOrEmpty(IsFeature) && IsFeature == "true")
+            {
+                items = items.Where(x => x.IsFeature == true);
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
             return View(items);
         }
-
+       
         public ActionResult Detail(string alias, int id)
         {
             var item = db.Products.Find(id);
@@ -35,12 +61,31 @@ namespace WebBanHangOnline.Controllers
             ViewBag.CountReview = countReview;
             return View(item);
         }
-        public ActionResult ProductCategory(string alias, int id)
+        public ActionResult ProductCategory(string alias, int id, int? page, string Searchtext, string IsHot, string IsSale, string IsFeature)
         {
-            var items = db.Products.ToList();
+            var pageSize = 8;
+            IEnumerable<Product> items = db.Products.ToList();
             if (id > 0)
             {
                 items = items.Where(x => x.ProductCategoryId == id).ToList();
+            }
+            //khung tìm kiếm
+            if (!string.IsNullOrEmpty(Searchtext))
+            {
+                items = items.Where(x => x.Alias.Contains(Searchtext) || x.Title.Contains(Searchtext));
+            }
+            //lọc theo tiêu chí
+            if (!string.IsNullOrEmpty(IsHot) && IsHot == "true")
+            {
+                items = items.Where(x => x.IsHot == true && x.ProductCategoryId == id);
+            }
+            if (!string.IsNullOrEmpty(IsSale) && IsSale == "true")
+            {
+                items = items.Where(x => x.IsSale == true && x.ProductCategoryId == id);
+            }
+            if (!string.IsNullOrEmpty(IsFeature) && IsFeature == "true")
+            {
+                items = items.Where(x => x.IsFeature == true && x.ProductCategoryId == id);
             }
             var cate = db.ProductCategories.Find(id);
             if(cate != null)
@@ -48,11 +93,17 @@ namespace WebBanHangOnline.Controllers
                 ViewBag.CateName = cate.Title;
             }
             ViewBag.CateId = id;
+            if (page == null)
+            {
+                page = 1;
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
             return View(items);
         }
         public ActionResult Partial_ItemsByCateId()
         {
-            var items = db.Products.Where(x => x.IsHome && x.IsActive).Take(12).ToList();
+            var items = db.Products.Where(x => x.IsHome && x.IsActive).Take(10).ToList();
             return PartialView(items);
         }
         public ActionResult Partial_ProductSales()
@@ -60,6 +111,5 @@ namespace WebBanHangOnline.Controllers
             var items = db.Products.Where(x => x.IsSale && x.IsActive).Take(12).ToList();
             return PartialView(items);
         }
-
     }
 }
