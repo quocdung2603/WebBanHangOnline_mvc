6d60e738c4d682524b21cfe3ba0022ba5168c501
quocdung2603 <2124802010008@student.tdmu.edu.vn>
2023-11-16 15:25:03 +0700

    16/11/23 3:26 PM push trước khi làm khuyến mãi ngày

---
+++ WebBanHangOnline/Areas/Admin/Controllers/TimePromotionController.cs
@@ -0,0 +1,31 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class TimePromotionController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/TimePromotion
+        public ActionResult Index(int? page)
+        {
+            IEnumerable<Product> items = db.Products.OrderByDescending(x => x.Id);
+            var pageSize = 10;
+            if (page == null)
+            {
+                page = 1;
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            return View(items);
+        }
+    }
+}
