68c8a954ae3240cdce883c46c31cabfa2f5e85ca
quocdung2603 <2124802010008@student.tdmu.edu.vn>
2023-12-07 11:24:23 +0700

    7/12/23 11:26 AM push

--- WebBanHangOnline/Areas/Admin/Controllers/HomeController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/HomeController.cs
@@ -42,7 +42,24 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
                     ViewBag.IdRoom = id;
                     return View(tmp);
                 }
-            }    
+            }
+            ViewBag.A = db.Orders.ToList().Count();
+            ViewBag.B = db.Orders.Where(x => x.OrderStatus == 3).Count();
+            double tmp1 = Convert.ToInt32(ViewBag.B)/ Convert.ToInt32(ViewBag.A) *100;
+            ViewBag.B = tmp1;
+            var userStore1 = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            var userManager1 = new UserManager<ApplicationUser>(userStore1);
+            var u = db.Users.ToList();
+            List<ApplicationUser> lstUser = new List<ApplicationUser>();
+            foreach(var item in u)
+            {
+                var ru = userManager1.GetRoles(item.Id).ElementAt(0);
+                if(ru == "Customer")
+                {
+                    lstUser.Add(item);
+                }
+            }
+            ViewBag.C = lstUser.Count();
             return View();
         }
        
